c7524b56491169081230f75aebb13fca4827a1bc
Cody Hastings <cody@hastings.email>
2022-04-04 16:15:06 -0500

    small changes(this is a bad commit message)

--- canvas.js
+++ canvas.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     var canvas = document.querySelector("canvas");
-    var gravity = 0.2;
-    var friction = 0.98;
+    const gravity = 0.2;
+    const friction = 0.98;
     var flag = true;
     var kill = false;
-    var c = canvas.getContext("2d");
-    // var mouse = {
-    //     x: 1,
-    //     y: 1
-    // }
-    var colors = [
+    const c = canvas.getContext("2d");
+    var mouse = {
+        x: 1,
+        y: 1
+    }
+    const colors = [
         '#04e5de',
         '#421384',
         '#c7d9e0',
@@ -21,11 +21,11 @@ $(document).ready(function() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
     });
-    // window.addEventListener("mousemove", function(event) {
-    //     // console.log(particleArray)
-    //     mouse.x = event.x;
-    //     mouse.y = event.y;
-    // })
+    window.addEventListener("mousemove", function(event) {
+        // console.log(particleArray)
+        mouse.x = event.x;
+        mouse.y = event.y;
+    })
     function randomIntFromRange(min,max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
@@ -33,39 +33,39 @@ $(document).ready(function() {
         return colors[Math.floor(Math.random() * colors.length)];
     }
 
-    // function Particle(x, y, radius, color) {
-    //     this.x = x;
-    //     this.y = y;
-    //     this.radius = radius;
-    //     this.color = color;
-    //     this.radians = Math.random() * Math.PI * 2;
-    //     this.velocity = 0.05;
-    //     this.distanceFromCenter = { x: randomIntFromRange(20, 70), y: randomIntFromRange(20,100)}
-    //     this.update = function() {
-    //         var lastPoint = {x: this.x, y: this.y};
-    //         this.radians += this.velocity
-    //         this.x = mouse.x + Math.cos(this.radians) * this.distanceFromCenter.x;
-    //         this.y = mouse.y + Math.sin(this.radians) * this.distanceFromCenter.y;
-    //
-    //
-    //         this.draw(lastPoint);
-    //     }
-    //
-    //     this.draw = function(lastPoint) {
-    //         c.beginPath();
-    //         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    //         c.fillStyle = this.color;
-    //         c.fill();
-    //         c.closePath();
-    //         c.beginPath();
-    //         c.strokeStyle = this.color;
-    //         c.lineWidth = this.radius * 2;
-    //         // c.moveTo(lastPoint.x, lastPoint.y)
-    //         c.lineTo(this.x, this.y)
-    //         c.stroke();
-    //         c.closePath();
-    //     };
-    // }
+    function Particle(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.radians = Math.random() * Math.PI * 2;
+        this.velocity = 0.05;
+        this.distanceFromCenter = { x: randomIntFromRange(20, 70), y: randomIntFromRange(20,100)}
+        this.update = function() {
+            var lastPoint = {x: this.x, y: this.y};
+            this.radians += this.velocity
+            this.x = mouse.x + Math.cos(this.radians) * this.distanceFromCenter.x;
+            this.y = mouse.y + Math.sin(this.radians) * this.distanceFromCenter.y;
+
+
+            this.draw(lastPoint);
+        }
+
+        this.draw = function(lastPoint) {
+            c.beginPath();
+            c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            c.fillStyle = this.color;
+            c.fill();
+            c.closePath();
+            c.beginPath();
+            c.strokeStyle = this.color;
+            c.lineWidth = this.radius * 2;
+            // c.moveTo(lastPoint.x, lastPoint.y)
+            c.lineTo(this.x, this.y)
+            c.stroke();
+            c.closePath();
+        };
+    }
 
 
     function Ball(x, y, dx, dy, radius, color) {
@@ -108,54 +108,58 @@ $(document).ready(function() {
         this.draw = function() {
             c.beginPath();
             c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            c.stroke();
             c.fillStyle = this.color;
             c.fill();
-            c.stroke();
             c.closePath();
         };
     }
     let ballArray = [];
     function init() {
-        var radius = randomIntFromRange(8, 20);
-        var x = randomIntFromRange(radius, canvas.width - radius);
-        var y = randomIntFromRange(0, canvas.height - radius);
-        var dx = randomIntFromRange(-3, 3)
-        var dy = randomIntFromRange(-2, 2)
-        ballArray.push(new Ball(x, y, dx, dy, radius, randomColor(colors)));
+        let radius = randomIntFromRange(8, 20);
+        let x = randomIntFromRange(radius, canvas.width - radius);
+        let y = randomIntFromRange(0, canvas.height - radius);
+        let dx = randomIntFromRange(-3, 3)
+        let dy = randomIntFromRange(-2, 2)
+        let color = randomColor(colors)
+        ballArray.push(new Ball(x, y, dx, dy, radius, color));
     }
     function initBigBall(){
-        var radius = 100;
-        var x = randomIntFromRange(radius, canvas.width - radius);
-        var y = randomIntFromRange(0, canvas.height - radius);
-        var dx = randomIntFromRange(-3, 3)
-        var dy = randomIntFromRange(-2, 2)
-        ballArray.push(new Ball(x, y, dx, dy, radius, randomColor(colors)));
+        let radius = 100;
+        let x = randomIntFromRange(radius, canvas.width - radius);
+        let y = randomIntFromRange(0, canvas.height - radius);
+        let dx = randomIntFromRange(-3, 3)
+        let dy = randomIntFromRange(-2, 2)
+        let color = randomColor(colors)
+        ballArray.push(new Ball(x, y, dx, dy, radius, color));
     }
     function init300Balls(){
-        for (var i = 0; i < 300; i++) {
-            var radius = randomIntFromRange(4, 12);
-            var x = randomIntFromRange(radius, canvas.width - radius);
-            var y = 400;
-            var dx = randomIntFromRange(-3, 3)
-            var dy = randomIntFromRange(-2, 2)
-            ballArray.push(new Ball(x, y, dx, dy, radius, randomColor(colors)));
+        for (let i = 0; i < 300; i++) {
+            let radius = randomIntFromRange(4, 12);
+            let x = randomIntFromRange(radius, canvas.width - radius);
+            let y = 400;
+            let dx = randomIntFromRange(-3, 3)
+            let dy = randomIntFromRange(-2, 2)
+            let color = randomColor(colors)
+            ballArray.push(new Ball(x, y, dx, dy, radius, color));
         }
     }
-    // var particleArray = []
-    // function initParticle(){
-    //     // console.log(particleArray)
-    //     for (var i = 0; i < 4; i++) {
-    //         x = canvas.width /2 + randomIntFromRange(-5, 5);
-    //         y = canvas.height /2 + randomIntFromRange(-5, 5);
-    //         radius = randomIntFromRange(5, 10)
-    //         particleArray.push(new Particle(x, y, radius, randomColor(colors)));
-    //
-    //     }
-    // }
+    var particleArray = []
+    function initParticle(){
+        // console.log(particleArray)
+        for (var i = 0; i < 4; i++) {
+            x = canvas.width /2 + randomIntFromRange(-5, 5);
+            y = canvas.height /2 + randomIntFromRange(-5, 5);
+            radius = randomIntFromRange(5, 10)
+            particleArray.push(new Particle(x, y, radius, randomColor(colors)));
+
+        }
+    }
     function animate() {
         requestAnimationFrame(animate);
-        c.fillStyle = 'rgba(0, 0, 0, 0.05)'
-        c.fillRect(0, 0, canvas.width, canvas.height)
+        // c.fillStyle = 'rgba(0, 0, 0, 0.05)'
+        // c.fillRect(0, 0, innerWidth, innerHeight)
+        c.clearRect(0, 0, innerWidth , innerHeight);
         if (flag === false){
             for (let i = 0; i < ballArray.length; i++) {
                 ballArray[i].vacuum();
@@ -180,7 +184,7 @@ $(document).ready(function() {
     function vacuum(){
         flag = false;
     }
-    // initParticle();
+    initParticle();
     animate();
     $("#addBall").click(init);
     $("#bigBall").click(initBigBall);
